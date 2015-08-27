8e140e9364f216aa34d60f0c4209bd445e0bbba3
Krystian Gwizdała <krystian@copper.io>
2015-08-27 12:23:46 +0200

    Added basic demo

---
+++ demo/demo.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('angularFilepickerExample', ['ngRoute', 'angular-filepicker'])
+.config(function ($routeProvider, angularFilepickerProvider) {
+    angularFilepickerProvider.setKey('AwMr7Yc2nQX2zdOcs5Q1Az');
+
+    $routeProvider
+    .when('/', {
+        templateUrl: 'templates/examples.html',
+        controller: 'ExamplesCtrl'
+    })
+    .when('/gallery', {
+        templateUrl: 'templates/gallery.html',
+        controller: 'GalleryCtrl'
+    })
+    .when('/about', {
+        templateUrl: 'templates/about.html',
+        controller: 'GalleryCtrl'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+})
+.controller('GalleryCtrl', function ($scope, filepickerService, $window) {
+    $scope.files = JSON.parse($window.localStorage.getItem('files') || '[]');
+    filepickerService.setKey('Av6PexkFtTuK4YXaE3T93z');
+
+    $scope.pickFile = pickFile;
+
+    $scope.onSuccess = onSuccess;
+
+    function pickFile(){
+        filepickerService.pick(
+            {mimetype: 'image/*'},
+            onSuccess
+        );
+    };
+
+    function onSuccess(Blob){
+        $scope.files.push(Blob);
+        $window.localStorage.setItem('files', JSON.stringify($scope.files));
+    };
+})
+.controller('ExamplesCtrl', function ($scope, filepickerService, $window) {
+    
+});
